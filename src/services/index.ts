4cd77ac44ca64036fda75bed5c6be12d800fb9bb
Alexis JUILLARD <62006143+alexjuillardoff@users.noreply.github.com>
2025-08-27 13:24:17 +0200

    chore: remove blog feature (#97)

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,3 @@
 export { default as RollerCoasterService } from './roller-coaster-service';
 export { default as ThemeParkService } from './theme-park-service';
 export { default as ScrapeService } from './scrape-service';
-export { default as BlogService } from './blog-service';
